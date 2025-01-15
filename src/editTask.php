db5cacefdeb36ddda74fdce99e60d5bd6cda7e82
Chokladglasyr <lim_ida@live.com>
2025-01-15 15:21:39 +0100

    added a missing div for styling

--- src/editTask.php
+++ src/editTask.php
@@ -3,19 +3,21 @@ if(isset($_GET['taskID'])){
     $task = getTaskOne($_GET['taskID']);
 }
 ?>
-<form class="editTask" action="oneList.php?listID=<?= $listTitle['listID'];?>" method="POST">
-<h2>Edit</h2>
-    <div>
-        <!-- Input for title, set placeholder value as title from dB -->
-        <label>Task</label>
-        <input type="text" name="title" placeholder="Title" value="<?=$task['title']?>">
-        <!-- Give ID to list so function knows where to update -->
-        <input type="hidden" name="id" value="<?=$task['taskID']?>">
-    </div>
-    <!-- Button to edit changes -->
-    <div>
-        <button class="BTNedit" type="submit" name="crud" value="editTask">Edit</button>
-        <!-- Button to cancel -->
-        <button class="BTNcancel" type="submit">Cancel</button>
-    </div>
-</form>
+<div class="editList">
+    <form class="editTask" action="oneList.php?listID=<?= $listTitle['listID'];?>" method="POST">
+    <h2>Edit</h2>
+        <div>
+            <!-- Input for title, set placeholder value as title from dB -->
+            <label>Task</label>
+            <input type="text" name="title" placeholder="Title" value="<?=$task['title']?>">
+            <!-- Give ID to list so function knows where to update -->
+            <input type="hidden" name="id" value="<?=$task['taskID']?>">
+        </div>
+        <!-- Button to edit changes -->
+        <div>
+            <button class="BTNedit" type="submit" name="crud" value="editTask">Edit</button>
+            <!-- Button to cancel -->
+            <button class="BTNcancel" type="submit">Cancel</button>
+        </div>
+    </form>
+</div>
